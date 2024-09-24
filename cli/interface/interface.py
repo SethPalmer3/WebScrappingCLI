fca19a5cf2f53403cbed80fdab3f65b45dd81a2e
SethPalmer3 <elderlord30@gmail.com>
2024-09-24 16:31:19 -0700

    Changed structure of CLI system. Using a messaging system TODO: refactor into seperate files

--- cli/interface/interface.py
+++ cli/interface/interface.py
@@ -1,4 +1,10 @@
+from enum import Enum
+# import os
 import os
+import sys
+from typing import Any
+
+LOGGING = False
 
 class HistoryElement:
     def __init__(self, input_str: str, output_str: str | None) -> None:
@@ -12,57 +18,85 @@ class HistoryElement:
         else:
             return False
 
-class InputProcessor:
-    def process(self, controller: "CLInterface", consumable: str) -> HistoryElement:
-        if consumable == "stop":
-            controller.stop()
+class CLIMessages(Enum):
+    DATA = 'Data'
+    DISPLAY = 'Display'
+    ERROR = 'Error'
+    STOP = 'Stop'
 
-        return HistoryElement(consumable, consumable)
+class Message:
+    def __init__(self, source: "Messenger", destination: "Messenger", message: CLIMessages, message_data: list[Any]) -> None:
+        self.srcMessenger = source
+        self.destMessenger = destination
+        self.message = message
+        self.message_data = message_data
 
-class CommandHanlder:
-    def __init__(self, input_processor: InputProcessor) -> None:
-        self.input_processor = input_processor
+class Messenger:
+    def __init__(self) -> None:
+        self.receive_queue = []
+        self.name = "Messenger"
 
-    def consume(self, interface: "CLInterface", consumable: str) -> HistoryElement:
-        processor_output = self.input_processor.process(interface, consumable)
-        return processor_output
+    def receive(self, message: Message) -> None:
+        if LOGGING:
+            sys.stdout.write(f"LOG: {self.name} received message from {message.srcMessenger.name} to {message.destMessenger.name}\n")
+        self.receive_queue.append(message)
 
-class DisplayHandler:
-    def __init__(self, input_indicator: str, output_indicator: str) -> None:
-        self.line_indicator = input_indicator
-        self.output_indicator = output_indicator
-        self.history: list[HistoryElement] = []
-    
-    def add_history(self, history_element: HistoryElement) -> None:
-        self.history.append(history_element)
+    def send_to(self, message: Message) -> None:
+        if LOGGING:
+            sys.stdout.write(f"LOG: sending message from {message.srcMessenger.name} to {message.destMessenger.name}\n")
+        message.destMessenger.receive(message)
 
-    def display(self) -> None:
-        os.system("clear")
+class Displayer:
+    def __init__(self, user_input_marker: str, respond_marker: str) -> None:
+        self.history = []
+        self.user_input_marker = user_input_marker
+        self.respond_marker = respond_marker
+
+    def add_history_element(self, input_str: str, output_str: str | None) -> None:
+        self.history.append(HistoryElement(input_str, output_str))
+
+    def display(self):
+        os.system('clear')
         for h in self.history:
-            print(f"{h.input_str}\n{self.output_indicator}{h.output_str}")
+            if isinstance(h, HistoryElement):
+                sys.stdout.write(f"{h.input_str}\n{self.respond_marker}{h.output_str}\n")
+            else:
+                sys.stdout.write(f"{h.__str__()}\n")
+        sys.stdout.write(self.user_input_marker)
+        sys.stdout.flush()
 
-        print(self.line_indicator, end="")
+    def display_message(self, message: str):
+        sys.stdout.flush()
+        sys.stdout.write(f"\033[1m{message}\033[0m\n")
 
-    def stop_message(self) -> None:
-        os.system("clear")
-        print("Stopping...")
+class CommandMessenger(Messenger):
+    def __init__(self) -> None:
+        super().__init__()
+        self.name = "CommandMessenger"
 
-class CLInterface:
-    def __init__(self, input_handler: CommandHanlder, output_handler: DisplayHandler) -> None:
-        self.input_handler = input_handler
-        self.output_handler = output_handler
-        self.run_state = False
+    def receive(self, message: Message) -> None:
+        if LOGGING:
+            sys.stdout.write(f"LOG: {self.name} received message from {message.srcMessenger.name} to {message.destMessenger.name}\n")
+        new_message = Message(message.destMessenger, message.srcMessenger, CLIMessages.DISPLAY, message.message_data)
+        self.send_to(new_message)
 
-    def loop(self):
-        self.run_state = True
-        while self.run_state:
-            # Clear the terminal
-            self.output_handler.display()
-            print(f"Current run state: {self.run_state}")
-            self.input = input()
-            h_elem = self.input_handler.consume(self, self.input)
-            self.output_handler.add_history(h_elem)
-    
-    def stop(self):
-        self.run_state = False
-        self.output_handler.stop_message()
+class UserInterfaceMessenger(Messenger):
+    def __init__(self, displayer: Displayer, commander: CommandMessenger) -> None:
+        super().__init__()
+        self.displayer = displayer
+        self.commander = commander
+        self.name = "UserInterfaceMessenger"
+
+    def listen(self) -> None:
+        while True:
+            self.displayer.display()
+            usr_input = sys.stdin.readline().strip()
+            cmnd_message = Message(self, self.commander, CLIMessages.DATA, [usr_input])
+            self.send_to(cmnd_message)
+            return_message = self.receive_queue.pop(0)
+            if return_message.message == CLIMessages.STOP:
+                self.displayer.display_message("Shutting down...")
+                break
+            self.displayer.add_history_element(usr_input, return_message.message_data[0])
+
+
